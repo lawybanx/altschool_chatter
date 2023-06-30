8c465fcd37b348b44cd08c8407e7210b141a20fc
Muhammad-awwal Bankole <awwalbankole@gmail.com>
2023-07-01 00:55:57 +0100

    troubleshooting markdown renderer

--- src/features/posts/utils/MarkdownRenderer.jsx
+++ src/features/posts/utils/MarkdownRenderer.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import { Box, useColorModeValue } from '@chakra-ui/react';
 import MarkdownPreview from '@uiw/react-markdown-preview';
 
 const MarkdownRenderer = ({ content }) => {
-  return <MarkdownPreview source={content} />;
+  const theme = useColorModeValue('light', 'dark');
+
+  return (
+    <Box p={{ base: '1rem', md: '1.5rem' }}>
+      <MarkdownPreview
+        source={content}
+        wrapperElement={{
+          'data-color-mode': theme,
+        }}
+        style={{
+          padding: '1rem 2rem',
+        }}
+      />
+    </Box>
+  );
 };
 
 export default MarkdownRenderer;
