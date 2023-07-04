ba7a2b7d1d4c209aa98bd501294de6fbeb490451
Muhammad-awwal Bankole <awwalbankole@gmail.com>
2023-07-04 14:27:30 +0100

    markdown preview styling

--- src/features/posts/utils/MarkdownRenderer.jsx
+++ src/features/posts/utils/MarkdownRenderer.jsx
@@ -1,32 +1,24 @@
 import React from 'react';
-import { Box, useColorModeValue } from '@chakra-ui/react';
+import { useColorModeValue } from '@chakra-ui/react';
 import MarkdownPreview from '@uiw/react-markdown-preview';
 import remarkGemoji from 'remark-gemoji';
 import remarkTypograf from '@mavrin/remark-typograf';
 import remarkIns from 'remark-ins';
-import remarkSubSuper from 'remark-sub-super';
 
 const MarkdownRenderer = ({ content }) => {
   const theme = useColorModeValue('light', 'dark');
 
   return (
-    <Box p={{ base: '1rem', md: '1.5rem' }}>
-      <MarkdownPreview
-        source={content}
-        wrapperElement={{
-          'data-color-mode': theme,
-        }}
-        style={{
-          padding: '1rem 2rem',
-        }}
-        remarkPlugins={[
-          remarkGemoji,
-          remarkTypograf,
-          remarkIns,
-          // remarkSubSuper,
-        ]}
-      />
-    </Box>
+    <MarkdownPreview
+      source={content}
+      wrapperElement={{
+        'data-color-mode': theme,
+      }}
+      style={{
+        padding: '1.5rem',
+      }}
+      remarkPlugins={[remarkGemoji, remarkTypograf, remarkIns]}
+    />
   );
 };
 
